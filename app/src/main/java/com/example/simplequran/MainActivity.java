44258bb1d085e0abe2618b6919b7ec9f023c8d59
Aqib Amin <aqibamin2002@gmail.com>
2022-12-05 18:23:31 +0500

    backend implemented

--- app/src/main/java/com/example/simplequran/MainActivity.java
+++ app/src/main/java/com/example/simplequran/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.simplequran;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn;
+    TextView txtSurah,txtVerse,txtDisplay;
+    QDH qdh = new QDH();
+    QuranArabicText txt = new QuranArabicText();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.btn_display);
+        txtSurah=findViewById(R.id.txt_suran_no);
+        txtVerse=findViewById(R.id.txt_verse_no);
+        txtDisplay=findViewById(R.id.txt_verse);
+        btn.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        if(R.id.btn_display == view.getId())
+        {
+            int surah = Integer.parseInt(txtSurah.getText().toString());
+            int verse = Integer.parseInt(txtVerse.getText().toString());
+            int startingVerse = qdh.SSP[surah - 1];
+            String verseStr = txt.QuranArabicText[startingVerse + verse - 1 ];
+            txtDisplay.setText(verseStr);
+        }
+    }
+
+
 }
