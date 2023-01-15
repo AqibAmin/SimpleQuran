71a9b51d139fb5f699bd52576537652fac22df4b
Aqib Amin <aqibamin2002@gmail.com>
2023-01-15 19:58:37 +0500

    Landscape qualifier and git intent added

--- app/src/main/java/com/example/simplequran/MainActivity.java
+++ app/src/main/java/com/example/simplequran/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.simplequran;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button btn;
+    ImageButton btn_git;
     TextView txtSurah,txtVerse,txtDisplay;
     QDH qdh = new QDH();
     QuranArabicText txt = new QuranArabicText();
@@ -17,9 +21,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn=findViewById(R.id.btn_display);
+        btn_git=findViewById(R.id.btn_img);
         txtSurah=findViewById(R.id.txt_suran_no);
         txtVerse=findViewById(R.id.txt_verse_no);
         txtDisplay=findViewById(R.id.txt_verse);
+        btn_git.setOnClickListener(this);
         btn.setOnClickListener(this);
     }
 
@@ -33,7 +39,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             String verseStr = txt.QuranArabicText[startingVerse + verse - 1 ];
             txtDisplay.setText(verseStr);
         }
+        if(R.id.btn_img == view.getId())
+        {
+            Uri webpage = Uri.parse("https://github.com/AqibAmin/SimpleQuran");
+            Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+            startActivity(intent);
+        }
     }
-
-
 }
